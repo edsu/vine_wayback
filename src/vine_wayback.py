ac542dfbb3aa0c8ca5c076c7c07cdbb8e2169596
Ed Summers <ehs@pobox.com>
2025-01-18 17:46:57 -0500

    Add some HTTP retries
    Use a requests Session to pipeline requests over the same connection,
    and add some retry behavior when the connection to archive.org fails.

--- src/vine_wayback.py
+++ src/vine_wayback.py
@@ -1,14 +1,13 @@
 import argparse
 import json
+import logging
 import re
+import time
 from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
 from wayback import WaybackClient
-
-wayback = WaybackClient()
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -17,19 +16,20 @@ def main():
     )
     parser.add_argument("-q", "--quiet", help="Suppress output")
     parser.add_argument("-o", "--output", help="A directory to write the output files")
+    parser.add_argument("-s", "--sleep", type=float, default=0.5, help="Sleep number seconds between requests to archive.org (default is 0.5)")
     args = parser.parse_args()
 
-    download(args.vine_url, quiet=args.quiet, output_dir=args.output)
+    download(args.vine_url, quiet=args.quiet, output_dir=args.output, sleep=args.sleep)
 
 
-def download(vine_url: str, metadata=False, quiet=False, output_dir=None):
+def download(vine_url: str, metadata=False, quiet=False, output_dir=None, sleep=0.5):
     m = re.match(r"https://vine.co/v/(.+)", vine_url)
     if not m:
         print(f"{vine_url} doesn't look like a Vine URL")
         return
     vine_id = m.group(1)
 
-    v = vine(vine_url)
+    v = vine(vine_url, sleep)
     if v is None and not quiet:
         print(f"The video for {vine_url} wasn't found in the Wayback Machine :(")
         return
@@ -53,10 +53,13 @@ def download(vine_url: str, metadata=False, quiet=False, output_dir=None):
         print(f"💾 saved {vine_url} to {output_dir.absolute()}")
 
 
-def vine(vine_url) -> dict:
+def vine(vine_url, sleep=0.5) -> dict:
     # go through each snapshot of the vine in the Wayback machine
     for vine_page in wayback.search(vine_url):
-        resp = requests.get(vine_page.raw_url)
+        time.sleep(sleep) # avoid getting blocked by archive.org
+
+        # get the raw html for the vine page
+        resp = get(vine_page.raw_url)
         if resp.status_code != 200:
             continue
 
@@ -72,7 +75,9 @@ def vine(vine_url) -> dict:
 
         # look for a snapshot of the video in the Wayback Machine
         for video in wayback.search(video_url):
-            resp = requests.get(video.raw_url)
+            time.sleep(sleep) # avoid getting blocked by archive.org
+
+            resp = get(video.raw_url)
             if resp.status_code == 200:
                 vine["video_raw"] = resp.content
                 break
@@ -157,3 +162,26 @@ def write_html(path, vine):
 </html>
 """
     open(path, "w").write(html)
+
+
+def get_requests_session():
+    http = requests.Session()
+    http.headers.update({"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14.7; rv:134.0) Gecko/20100101 Firefox/134.0"})
+    return http
+
+
+def get(url, retries=5):
+    global http
+
+    try:
+        return http.get(url)
+    except requests.exceptions.ConnectionError as e:
+        logging.error(e)
+        if retries > 0:
+            # recreate our http session
+            http = get_requests_session()
+            return get(url, retries - 1)
+
+wayback = WaybackClient()
+
+http = get_requests_session()
